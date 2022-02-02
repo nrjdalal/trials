402aa44776385cb726df584977476d9128c248c4
nrjdalal <nrjdalal@nrjdalal.local>
2022-02-02 15:00:39 +0530

    Wed  2 Feb 2022 15:00:39 IST

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,77 @@
 import Head from 'next/head'
+import { FaInstagram, FaTwitter } from 'react-icons/fa'
 
-export default function Home() {
+const Profiles = (props: any) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <>
+      <div className="relative flex flex-col items-center p-6 mt-12 border-2 border-blue-400 shadow-xl rounded-2xl">
+        <div className="absolute w-24 h-24 overflow-hidden border-2 border-blue-400 rounded-full -top-10">
+          <img
+            className="object-contain object-center aspect-square"
+            src={props.image}
+            alt=""
+          />
+        </div>
+
+        <h2 className="pt-12 text-3xl font-medium">{props.name}</h2>
+        <p className="text-xl">{props.price}</p>
+        <div className="flex gap-4 pt-6">
+          {/* <div className="rounded-full border-2 border-blue-400 p-1.5 text-blue-400"> */}
+          <FaInstagram className="w-8 h-8" />
+          {/* </div> */}
+          {/* <div className="rounded-full border-2 border-blue-400 p-1.5 text-blue-400"> */}
+          <FaTwitter className="w-8 h-8" />
+          {/* </div> */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+const index = () => {
+  return (
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="w-screen h-screen py-20 bg-slate-100">
+        <div className="grid max-w-screen-lg grid-cols-1 gap-6 px-6 mx-auto sm:grid-cols-2 lg:grid-cols-3">
+          <Profiles
+            image="https://source.unsplash.com/random/500x500/?guy,alfa"
+            name="Abhishek"
+            price="$300 for 60 minutes"
+          />
+          <Profiles
+            image="https://source.unsplash.com/random/500x500/?guy,beta"
+            name="Neeraj"
+            price="$300 for 60 minutes"
+          />
+          <Profiles
+            image="https://source.unsplash.com/random/500x500/?girl,alfa"
+            name="Disha"
+            price="$800 for 60 minutes"
+          />
+          <Profiles
+            image="https://source.unsplash.com/random/500x500/?girl,beta"
+            name="Nikita"
+            price="$700 for 60 minutes"
+          />
+          <Profiles
+            image="https://source.unsplash.com/random/500x500/?guy,charlie"
+            name="Saurav"
+            price="$400 for 60 minutes"
+          />
+          <Profiles
+            image="https://source.unsplash.com/random/500x500/?guy,delta"
+            name="Anmol"
+            price="$800 for 60 minutes"
+          />
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   )
 }
+
+export default index
