782e8d330e926c992a29a892e395769a29bad757
nrjdalal <nrjdalal@psycho.local>
2022-02-05 01:15:26 +0530

    Sat  5 Feb 2022 01:15:26 IST

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="w-screen h-screen overflow-hidden">
+      <div className="w-screen h-screen overflow-x-hidden">
         <Component {...pageProps} />
       </div>
     </>
